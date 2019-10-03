dca5e2d63d6e91e642b0a5989f9607456a8615f5
Nitin Chandran Nair <nitin.dreamern89@gmail.com>
2019-10-03 15:40:35 +0000

    Added lightbox modal to show the generated source code, applied CSS, added new JS functions

--- tableGen.js
+++ tableGen.js
@@ -6,7 +6,12 @@ let app = new Vue({
       columns: '',
       rows: '',
       tableString: '',
-      header: false
+      header: false,
+      tableSourceCode: '',
+      showTableSourceCode: false,
+      copyStatus: false,
+      copyStatusText: '',
+      copyStatusClass: ''
     }
   },
   methods: {
@@ -45,11 +50,38 @@ let app = new Vue({
     changeValue() {
       this.preHTML()
     },
-    openInNewTab() {
-      let newWindow = window.open("about:blank");
-      let string = '<pre>' + this.tableString.replace(/&/g, '&amp;').replace(/</g, '&lt;') + '</pre>';
-      newWindow.document.write(string)
-      newWindow.document.close()
+    openModal() {
+      // Opens the modal and set flag
+      this.showTableSourceCode = true;
+      let string = '<code>' + this.tableString.replace(/&/g, '&amp;').replace(/</g, '&lt;') + '</code>';
+      this.tableSourceCode = string;
+    },
+    closeModal() {
+      // Closes the modal and reset flags as well as status content
+      this.showTableSourceCode = false;
+      this.copyStatus = false;
+      this.copyStatusClass = '';
+      this.copyStatusText = '';
+    },
+    copyCode() {
+      // Copies the content of 'generatedSourceCode' <div> to user's clipboard
+      let generatedSourceCodeStr = document.querySelector('#generatedSourceCode');
+      let range = document.createRange();
+      range.selectNode(generatedSourceCodeStr);
+      window.getSelection().addRange(range);
+
+      try {
+        // Execute copy command and set status content
+        this.copyStatus = document.execCommand('copy');
+        this.copyStatusClass = this.copyStatus ? 'has-background-success' : 'has-background-danger';
+        this.copyStatusText = 'Copied!'
+      } catch(err) {
+        // Copy didn't work, reset status content
+        this.copyStatus = false;
+        this.copyStatusClass = '';
+        this.copyStatusText = 'Something went wrong!'
+      }
+      window.getSelection().removeAllRanges();
     }
   }
 })
